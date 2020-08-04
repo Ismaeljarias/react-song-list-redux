5dc12943a5301663c58abd777bbca80a87a1695d
Ismael Arias <ismaeljarias@gmail.com>
2020-08-04 07:08:55 -0400

    Complete a simple song list with redux

---
+++ src/reducers/index.js
@@ -0,0 +1,37 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    {
+      title: "No Scrubs",
+      duration: "4:05",
+    },
+    {
+      title: "Macarena",
+      duration: "2:30",
+    },
+    {
+      title: "All Star",
+      duration: "3:15",
+    },
+    {
+      title: "I Want It That Way",
+      duration: "1:45",
+    },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  switch (action.type) {
+    case "SONG_SELECTED":
+      return action.payload;
+
+    default:
+      return selectedSong;
+  }
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
